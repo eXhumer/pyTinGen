17ae29847f8ab81008727a723cab151b389041ee
Anon Ymous <5151437-eXhumer@users.noreply.gitlab.com>
2020-02-18 10:58:17 +0000

    Script cleanup in attempt to implement to main script.

--- public_folder_index_generator.py
+++ public_folder_index_generator.py
@@ -31,7 +31,41 @@ class GdriveSession:
 			req_headers.update({"Referer": options.get("referer")})
 		return self.session.request(method, url, headers=req_headers, verify=False, stream=True)
 
-def getJson(buf):
+	def get_files(self, id, j):
+		files = []
+		page_token = None
+		try:
+			for _ in range(100):
+				url = 'https://clients6.google.com/drive/v2beta/files?openDrive=false&reason=102&syncType=0&errorRecovery=false&q=trashed%20%3D%20false%20and%20%27' + id + '%27%20in%20parents&fields=kind%2CnextPageToken%2Citems(kind%2CfileSize%2Ctitle%2Cid)%2CincompleteSearch&appDataFilter=NO_APP_DATA&spaces=drive&maxResults=500&orderBy=folder%2Ctitle_natural%20asc&key=' + get_key(j)
+				
+				if page_token is not None:
+					url += '&pageToken=' + page_token
+				r = self.make_request('GET', url, referer='https://drive.google.com/open?id=' + id)
+				j2 = json.loads(r.text)
+				print(j2)
+				for a in j2['items']:
+					if a['kind'] != "drive#file":
+						continue
+					if 'fileSize' in a:
+						files.append({'url': 'gdrive:' + a["id"] + '#' + urllib.parse.quote(a['title'], safe=''), 'size': int(a['fileSize'])})
+					else:
+						files.append({'url': 'gdrive:' + a["id"] + '#' + urllib.parse.quote(a['title'], safe='') })
+				if 'nextPageToken' not in j2:
+					break
+				page_token = j2['nextPageToken']
+		except:
+			pass
+		return files
+
+	def process_dirs(self, ids):
+		results = {"files": [], "success": "hello world"}
+		for id in ids:
+			url = 'https://drive.google.com/open?id=' + id
+			r = self.make_request('GET', url)
+			j = get_json(r.text)
+			results["files"] += self.get_files(id, j)
+
+def get_json(buf):
 	try:
 		start = buf.index('__initData = ') + len('__initData = ')
 		end = buf.index(';', start)
@@ -39,58 +73,19 @@ def getJson(buf):
 	except:
 		return None
 
-def getKey(j):
+def get_key(j):
 	try:
 		return j[0][9][32][35] # :nospies:
 	except:
 		return ''
 
-def getFiles(id, j, gdrive_session):
-	files = []
-	pageToken = None
-	try:
-		for _ in range(100):
-			url = 'https://clients6.google.com/drive/v2beta/files?openDrive=false&reason=102&syncType=0&errorRecovery=false&q=trashed%20%3D%20false%20and%20%27' + id + '%27%20in%20parents&fields=kind%2CnextPageToken%2Citems(kind%2CfileSize%2Ctitle%2Cid)%2CincompleteSearch&appDataFilter=NO_APP_DATA&spaces=drive&maxResults=500&orderBy=folder%2Ctitle_natural%20asc&key=' + getKey(j)
-			
-			if pageToken is not None:
-				url += '&pageToken=' + pageToken
-
-			r = gdrive_session.make_request('GET', url, referer='https://drive.google.com/open?id=' + id)
-
-			j2 = json.loads(r.text)
-			print(j2)
-
-			for a in j2['items']:
-				if a['kind'] != "drive#file":
-					continue
-				if 'fileSize' in a:
-					files.append({'url': 'gdrive:' + a["id"] + '#' + urllib.parse.quote(a['title'], safe=''), 'size': int(a['fileSize'])})
-				else:
-					files.append({'url': 'gdrive:' + a["id"] + '#' + urllib.parse.quote(a['title'], safe='') })
-			if 'nextPageToken' not in j2:
-				break
-			pageToken = j2['nextPageToken']
-	except:
-		pass
-
-	return files
-
-def processDir(id, gdrive_session):
-	url = 'https://drive.google.com/open?id=' + id
-	r = gdrive_session.make_request('GET', url)
-	j = getJson(r.text)
-	return getFiles(id, j, gdrive_session)
-
 if __name__ == "__main__":
 	if sys.argv[1] not in ["-h", "--help"]:
-		fileName = sys.argv[1]
+		file_name = sys.argv[1]
 		ids = sys.argv[2:]
-
-		result = {'files': [], 'success': 'hello world'}
-		for id in ids:
-			result['files'] += processDir(id, GdriveSession())
-
-		with open(fileName, 'w') as outfile:
-			json.dump(result, outfile)
+		gdrive_session = GdriveSession()
+		results = gdrive_session.process_dirs(ids)
+		with open(file_name, 'w') as outfile:
+			json.dump(results, outfile)
 	else:
 		print("Usage: python3 public_folder_index_generator.py [FOLDER_IDS_TO_SCAN [FOLDER_IDS_TO_SCAN ...]]")
