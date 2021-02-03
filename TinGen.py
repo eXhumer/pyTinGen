ec18007a3bc57f9214ab35584967a37730470304
eXhumer <eXhumer@member.fsf.org>
2021-02-02 21:35:01 -0700

    Add new option to generate token only * Generates a token compatiable with Tinfoil

--- TinGen.py
+++ TinGen.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
+from google.auth.credentials import Credentials
 from TinGen.utils import create_tinfoil_index
 from TinGen.utils import CompressionFlag
 from argparse import ArgumentParser
@@ -108,6 +109,18 @@ if __name__ == '__main__':
         help='Shares the index file that is uploaded to Google Drive',
     )
 
+    task_parser = parser.add_mutually_exclusive_group()
+    task_parser.add_argument(
+        '--auth',
+        action='store_true',
+        help='Run Google User Token authorize task if token doesn\'t exist',
+    )
+    task_parser.add_argument(
+        '--generator',
+        action='store_true',
+        help='Run index generation task',
+    )
+
     compression_parser = parser.add_mutually_exclusive_group()
     compression_parser.add_argument(
         '--zstandard',
@@ -133,67 +146,80 @@ if __name__ == '__main__':
         args.headless,
     )
 
-    print('Generating index')
-    generator.index_generator(
-        args.folder_ids,
-        args.recursion,
-        args.add_nsw_files_without_title_id,
-        args.add_non_nsw_files,
-    )
-
-    if args.success:
-        print('Adding success message to index')
-        generator.update_index_success_message(
-            args.success.replace('\\n', '\n').replace('\\t', '\t'),
+    if args.auth:
+        credentials = generator.gdrive_service._get_creds(
+            credentials=args.credentials,
+            token=args.token,
+            headless=args.headless,
         )
+        if isinstance(credentials, Credentials):
+            print('Token generated successfully!')
+        else:
+            raise RuntimeError('Unable to generate OAuth2 user credentials. ' +
+                               'Unable to continue!')
 
-    compression_flag = CompressionFlag.ZSTD_COMPRESSION
-
-    if args.zstandard:
-        compression_flag = CompressionFlag.ZSTD_COMPRESSION
-    elif args.zlib:
-        compression_flag = CompressionFlag.ZLIB_COMPRESSION
-    elif args.no_compress:
-        compression_flag = CompressionFlag.NO_COMPRESSION
-
-    print(f'Creating generated index to {args.index_file}')
-    if args.encrypt:
-        create_tinfoil_index(
-            generator.index,
-            Path(args.index_file),
-            compression_flag,
-            rsa_pub_key_path=Path(args.public_key) if args.public_key else None,
-            vm_path=Path(args.vm_file) if args.vm_file else None,
-        )
     else:
-        create_tinfoil_index(
-            generator.index,
-            Path(args.index_file),
-            compression_flag,
+        print('Generating index')
+        generator.index_generator(
+            args.folder_ids,
+            args.recursion,
+            args.add_nsw_files_without_title_id,
+            args.add_non_nsw_files,
         )
 
-    if args.share_files:
-        print('Sharing files in index')
-        for folder_id in args.folder_ids:
-            generator.gdrive_service.share_file(folder_id)
-        # generator.share_index_files()
+        if args.success:
+            print('Adding success message to index')
+            generator.update_index_success_message(
+                args.success.replace('\\n', '\n').replace('\\t', '\t'),
+            )
 
-    if args.upload_folder_id:
-        print(f'Uploading {args.index_file} to {args.upload_folder_id}')
-        generator.gdrive_service.upload_file(
-            args.index_file,
-            args.upload_folder_id,
-            args.share_uploaded_index,
-            args.new_upload_id,
-        )
+        compression_flag = CompressionFlag.ZSTD_COMPRESSION
 
-    if args.upload_to_my_drive:
-        print(f'Uploading {args.index_file} to \"My Drive\"')
-        generator.gdrive_service.upload_file(
-            args.index_file,
-            None,
-            args.share_uploaded_index,
-            args.new_upload_id,
-        )
+        if args.zstandard:
+            compression_flag = CompressionFlag.ZSTD_COMPRESSION
+        elif args.zlib:
+            compression_flag = CompressionFlag.ZLIB_COMPRESSION
+        elif args.no_compress:
+            compression_flag = CompressionFlag.NO_COMPRESSION
 
-    print('Index Generation Complete')
+        print(f'Creating generated index to {args.index_file}')
+        if args.encrypt:
+            create_tinfoil_index(
+                generator.index,
+                Path(args.index_file),
+                compression_flag,
+                rsa_pub_key_path=Path(args.public_key) if args.public_key else None,
+                vm_path=Path(args.vm_file) if args.vm_file else None,
+            )
+        else:
+            create_tinfoil_index(
+                generator.index,
+                Path(args.index_file),
+                compression_flag,
+            )
+
+        if args.share_files:
+            print('Sharing files in index')
+            for folder_id in args.folder_ids:
+                generator.gdrive_service.share_file(folder_id)
+            # generator.share_index_files()
+
+        if args.upload_folder_id:
+            print(f'Uploading {args.index_file} to {args.upload_folder_id}')
+            generator.gdrive_service.upload_file(
+                args.index_file,
+                args.upload_folder_id,
+                args.share_uploaded_index,
+                args.new_upload_id,
+            )
+
+        if args.upload_to_my_drive:
+            print(f'Uploading {args.index_file} to \"My Drive\"')
+            generator.gdrive_service.upload_file(
+                args.index_file,
+                None,
+                args.share_uploaded_index,
+                args.new_upload_id,
+            )
+
+        print('Index Generation Complete')
